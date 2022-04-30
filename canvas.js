77f1f9e561c6cb1bea3c5349c293c48026077df0
michaelpaulsen <michael.j.paulsen@gmail.com>
2022-04-30 14:11:50 -0700

    Update canvas.js
    ++ transform stack and implemented a PushTransfrom and PopTransform method [for pushing and poping from the transform stack respectivly] 
    --  old [non-working] setTransform method 
    ++ fill property that when set to true makes it so that the writing functions fill the shapes that they are writing 
    ** made it so that the functions use the fill property.

--- canvas.js
+++ canvas.js
@@ -1,19 +1,57 @@
+let toRads =  function(x){ return x* Math.PI/180;}; 
+    
 var Canvas = function(id){ 
+  let el =document.getElementById(id);  
 	var c = {
-		ctx:document.getElementById(id).getContext("2d"),
-		toRads(x){ return x* Math.PI/180;},
+    tfrStack:[], 
+    fill: false,
+    setFillStyle:function(color) {this.ctx.fillStyle = color;},
+    strokeWidth : function(width){this.ctx.lineWidth = width;},
+    startShape: function(){this.ctx.beginPath();},
+    endShape  : function(){this.ctx.closePath();},
+    pushTransform: function(){
+      
+      this.tfrStack.push(this.ctx.getTransform());
+      
+    },
+    popTransform: function(){
+      let t = this.tfrStack;
+      if(t){
+        this.ctx.setTransform(t); 
+      }else{
+        console.error("no transform to pop!"); 
+      }
+    },
+    width: el.offsetWidth,
+    height:el.offsetHeight, 
+		ctx: el.getContext("2d"),
+		translate :function(x, y){this.ctx.translate(x, y);},
+    rotate: function(x){this.ctx.rotate(x)}, 
+    strokeColor: function(color){
+      this.ctx.strokeStyle = color;  
+    },
 		rect:function(x,y,w,h){
-			this.ctx.strokeRect(x,y,w,h);
-		},
+			if(!this.fill){
+        this.ctx.strokeRect(x,y,w,h);
+		  }else{
+        this.ctx.fillRect(x,y,w,h); 
+      }
+    },
 		ellipse:function(){
+      /* 
+        x, y, radiusX, radiusY, rotation, startAngle, endAngle
+      */
 			var args = arguments;
 			this.ctx.beginPath();
+      if(args.length == 3){
+        this.ctx.ellipse(args[0], args[1], args[2],  args[2], 0,0,2*Math.PI);
+      }else 
 			if( args.length == 9){
 				this.ctx.ellipse(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8]);
 			}else if(args.length == 4 ){
 				this.ctx.ellipse(args[0], args[1], args[2], args[3],0,0,360);
 			}else{ 
-			console.error("ellipse takes ether 4 arguments or 9")
+			console.error("ellipse takes ether 3, 4 or 9 arguments")
 			}
 			this.ctx.stroke();
 		},
@@ -38,27 +76,25 @@ var Canvas = function(id){
 				this.ctx.setLineDash(arguments);
 			}
 		},
-		transform:function(hs,vs,hsk,vsk,dx,dy){
-		/**Horizontal scaling,  Vertical scaling Horizontal skewing Vertical skewing,dx, dy. */
-			this.ctx.transform(hs, vsk, hsk, vs, dx, dy);
-		},
-		setTransform: function(a,b,c,d,e,f){
-			if(arguments.length != 8){ 
-			a, d = 1;
-			b, c, e, f =0;
-			}
-			this.ctx.setTransform(a,b,c,d,e,f);
-		},
 		line:function(x1,y1,x2,y2){
-			this.ctx.beginPath();
 			this.ctx.moveTo(x1, y1);
 			this.ctx.lineTo(x2, y2); 
-			this.ctx.stroke();  
+			this.ctx.stroke();
 		},
-		triangle:function(x1,y1,x2,y2,x3,y3){
-			this.line(x1,y1,x2,y2); 
-			this.line(x2,y2,x3,y3); 
-			this.line(x1,y1,x3,y3); 
+		triangle:function(x1,y1,x2,y2,x3,y3, color){
+			let region = new Path2D(); 
+      region.moveTo(x1, y1);
+      region.lineTo(x2, y2);
+      region.lineTo(x3, y3);
+      
+      region.closePath();
+
+      if(this.fill){ 
+       this.ctx.fill(region, 'evenodd');
+      }else{
+        this.ctx.stroke(region); 
+      }
+        
 		}
 	}	
 	return  c;
